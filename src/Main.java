f7f21fa1e12d6b3d3ebabdf4caa7ef8b8041167f
Luigi Antonio Junior De Matteis <luigi.dematteis.linuxdev@gmail.com>
2023-08-02 09:49:11 +0300

    Add funny challenge
    Signed-off-by: Luigi Antonio Junior De Matteis <luigi.dematteis.linuxdev@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import static funny.Compression.betterCompression;
+
 public class Main {
     public static void main(String[] args) {
-
+        String string = betterCompression("a12b56c1");
+        System.out.println(string);
     }
 }
---
+++ src/funny/Compression.java
@@ -0,0 +1,51 @@
+package funny;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Compression {
+    public static String betterCompression(String s) {
+        char[] chars = s.toCharArray();
+        char currChar;
+        int value;
+        Map<Character, Integer> expr = new HashMap<>();
+
+        System.out.println(chars.length);
+        for (int i = 0; i < chars.length; i++) {
+            System.out.println(Character.isDigit(chars[i]));
+            if(Character.isDigit(chars[i])) {
+                continue;
+            } else {
+                currChar = chars[i];
+                System.out.println(currChar);
+            }
+            String valueToParse = "";
+            int j = i + 1;
+            while (j < chars.length && Character.isDigit(chars[j])) {
+                System.out.println(Character.isDigit(chars[j]));
+                valueToParse = valueToParse.concat(String.valueOf(chars[j]));
+                j++;
+            }
+            System.out.println("valueToParse=" + valueToParse);
+            value = Integer.parseInt(valueToParse);
+
+            if (expr.containsKey(currChar)) {
+                int sum = expr.get(currChar) + value;
+                expr.put(currChar, sum);
+            } else {
+                expr.put(currChar, value);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (Character key : expr.keySet()) {
+            System.out.println("key=" + key);
+            System.out.println("value=" + expr.get(key));
+            result.append(key.toString()).append(expr.get(key).toString());
+            System.out.println("result=" + result);
+        }
+
+        System.out.println(result);
+        return result.toString();
+    }
+}
