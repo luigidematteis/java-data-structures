70c020e3d8b899457db99a71496e0c116e9278ab
Luigi Antonio Junior De Matteis <luigi.dematteis.linuxdev@gmail.com>
2023-08-04 15:15:10 +0300

    trees: Binary Tree InOrder Traversal
    Signed-off-by: Luigi Antonio Junior De Matteis <luigi.dematteis.linuxdev@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import arrays.GroupAnagrams;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        GroupAnagrams groupAnagrams = new GroupAnagrams();
+        String[] strings = {"eat","tea","tan","ate","nat","bat"};
+        groupAnagrams.groupAnagrams(strings);
     }
 }
---
+++ src/trees/InorderTraversal.java
@@ -0,0 +1,59 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class InorderTraversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> treeInOrderValues = new ArrayList<>();
+        traversal(root, treeInOrderValues);
+        return treeInOrderValues;
+    }
+
+/**
+ *           9
+ *      4        20
+ *   1    6    15   170
+ *
+ */
+    private List<Integer> traversal(TreeNode node, List<Integer> treeInOrderValues) {
+        if (node == null) return treeInOrderValues;
+        if (node.left != null) {
+            traversal(node.left, treeInOrderValues);
+        }
+        treeInOrderValues.add(node.val);
+        if (node.right != null) {
+            traversal(node.right, treeInOrderValues);
+        }
+        return treeInOrderValues;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
