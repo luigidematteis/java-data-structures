d8b958464a14627b3ceb89c1371e2e05aebf0c42
Luigi Antonio Junior De Matteis <luigi.dematteis.linuxdev@gmail.com>
2023-08-04 12:00:39 +0300

    Clean up

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
-import arrays.MatrixZeroes;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static funny.Compression.betterCompression;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
