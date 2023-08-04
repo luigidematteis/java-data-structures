938a9aa1a20bb9f9b3fa54774f5e1aa43387a971
Luigi Antonio Junior De Matteis <luigi.dematteis.linuxdev@gmail.com>
2023-08-04 16:58:35 +0300

    trees: add BinarySearchTree
    Signed-off-by: Luigi Antonio Junior De Matteis <luigi.dematteis.linuxdev@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 import arrays.GroupAnagrams;
+import trees.BinarySearchTree;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        GroupAnagrams groupAnagrams = new GroupAnagrams();
-        String[] strings = {"eat","tea","tan","ate","nat","bat"};
-        groupAnagrams.groupAnagrams(strings);
+       BinarySearchTree tree = new BinarySearchTree();
+       tree.insert(15);
+       tree.insert(170);
+       tree.insert(9);
+       tree.insert(4);
+       tree.insert(20);
+       tree.insert(1);
+       tree.insert(6);
+       System.out.println(tree.listTree());
     }
 }
--- src/trees/BinarySearchTree.java
+++ src/trees/BinarySearchTree.java
@@ -1,17 +1,47 @@
 package trees;
 
+import java.util.List;
+
 public class BinarySearchTree {
 
     TreeNode root;
 
-    BinarySearchTree() {
+    public BinarySearchTree() {
         this.root = null;
     }
 
+    /**
+     *           9
+     *      4        20
+     *   1    6    15   170
+     *
+     */
     public void insert(int value) {
         TreeNode node = new TreeNode(value);
         if (this.root == null) {
             this.root = node;
+        } else {
+            TreeNode currentNode = this.root;
+            while (true) {
+                if (value < currentNode.val) {
+                    if (currentNode.left == null) {
+                        currentNode.left = node;
+                        return;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    if (currentNode.right == null) {
+                        currentNode.right = node;
+                        return;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
         }
+    }
+
+    public List<Integer> listTree() {
+        InorderTraversal traversal = new InorderTraversal();
+        return traversal.inorderTraversal(this.root);
     }
 }
